c1d1bebaa16d25abdbea0a5df5721fbabfa5c2cd
mistvan22 <pocokxtrame@gmail.com>
2016-12-13 08:25:28 +0100

    GA

--- wp-content/themes/Avada-Child-Theme/functions.php
+++ wp-content/themes/Avada-Child-Theme/functions.php
@@ -483,3 +483,22 @@ function custom_rss_content_type( $content_type, $type ) {
 	return $content_type;
 }
 add_filter( 'feed_content_type', 'custom_rss_content_type', 10, 2 );
+
+/* GOOGLE ANALYTICS */
+if( defined('DEVMODE') && DEVMODE === false ) {
+	function ga_tracking_code () {
+		?>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-24431206-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+		<?
+	}
+	add_action('wp_footer', 'ga_tracking_code');
+}
